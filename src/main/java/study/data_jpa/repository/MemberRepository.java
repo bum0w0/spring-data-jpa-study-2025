52ca5fc41ba0ad05bb4925b45939bb0146d83902
bum0w0 <kgy5988@naver.com>
2025-06-01 18:47:38 +0900

    공통 인터페이스 분석

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package study.data_jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.data_jpa.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.List;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
 }
